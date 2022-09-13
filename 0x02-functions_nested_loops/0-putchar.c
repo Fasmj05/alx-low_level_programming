f35bdb91fc6816ab5a9a8e2fd5ff8e962df3abbc
Fasmj05 <mbanefofrancis@gmail.com>
2022-09-13 12:33:36 -0700

    g

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+  * main - Entry point
+ * Return: Always return 0
+*/
+int _putchar(int _putchar);
+int main(void)
+{
+ _putchar("\displays the out _putchar");	
+    	   _putchar('_');
+	   _putchar('u');
+	   _putchar('t');
+	   _putchar('c');
+	   _putchar('h');
+	   _putchar('a');
+	   _putchar('r');
+	   _putchar('\n');
+	   return (0);
+}
