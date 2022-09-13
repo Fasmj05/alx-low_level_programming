1c0f993c1d3d8683171e97b7f0832df6cdb31511
Fasmj05 <mbanefofrancis@gmail.com>
2022-09-13 04:53:52 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "alx.h"
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always returns  */
+int main(void)
+{
+		_putchar('H');
+			_putchar('o');
+				_putchar('l');
+					_putchar('b');
+						_putchar('e');
+							_putchar('r');
+								_putchar('t');
+									_putchar('o');
+										_putchar('n');
+											_putchar('\n');
+												return (0);
+}
