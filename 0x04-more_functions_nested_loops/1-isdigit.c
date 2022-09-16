3eba9c40a78f82cce96b0ee190fd297a6014de4d
Fasmj05 <mbanefofrancis@gmail.com>
2022-09-15 17:15:47 -0700

    ₦

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- *  * _isdigit - checks if a character is digit
- *   * @c: carrier variable
- *    * Return: 1 if true, 0 if false
- *     */
+*  * _isdigit - checks if a character is digit
+*   * @c: carrier variable
+*    * Return: 1 if true, 0 if false
+*     */
 
 int _isdigit(int c)
 {
-	    if (c >= '0' && c <= '9')
-		            return (1);
+if (c >= '0' && c <= '9')
+return (1);
 
-	        else
-			        return (0);
+else
+t  add -A
+return (0);
+
 }
