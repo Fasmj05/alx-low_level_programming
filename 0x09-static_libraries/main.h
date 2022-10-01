b28dd24c0e4432d2559e58ffb51be3f3db2fefb1
Fasmj05 <mbanefofrancis@gmail.com>
2022-09-30 17:27:26 -0700

    g

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -22,5 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
-```
+#endift
